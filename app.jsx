7c9f8a18a144463613a1902338a0045752d9e001
Tatiana Mitrofanova <tatiana.mitrofanova@mgroupweb.com>
2018-02-16 20:25:49 +0200

    final

--- app.jsx
+++ app.jsx
@@ -3,7 +3,6 @@ import 'slick-carousel';
 import 'slick-carousel/slick/slick.scss';
 import 'slick-carousel/slick/slick-theme.scss';
 
-
 $(document).ready(() => {
   $('.testimonials__carousel').not('.slick-initialized').slick({
     dots: true,
@@ -12,4 +11,18 @@ $(document).ready(() => {
     verticalSwiping: true,
     infinite: false
   });
+});
+
+let collapseHeader = true;
+
+$('[data-action="collapse"]').on('click', function () {
+    let el = $(this).attr('data-target');
+
+    if(collapseHeader) {
+      $(el).collapse('hide');
+      collapseHeader = false;
+    } else {
+      $(el).collapse('show');
+      collapseHeader= true;
+    }
 });
